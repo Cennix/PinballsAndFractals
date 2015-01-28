046e960a9950930a30db82d64b6612670751264a
mbax2dh2 <daniel.hopkins@student.manchester.ac.uk>
2015-01-28 15:04:14 +0000

    Swapped back to using an abstract collision object

---
+++ src/main/java/com/mbax2dh2/PinballMachine/PinballComponents/Collider.java
@@ -0,0 +1,28 @@
+package com.mbax2dh2.PinballMachine.PinballComponents;
+
+import java.awt.*;
+
+/**
+ * Created by Daniel on 28/01/2015.
+ * Updated last on 28/01/2015
+ */
+public class Collider
+{
+    // the class to hold the methods used in the collision map for the project
+    void response() {}
+
+    boolean collided(Pinball ball)
+    {
+        return false;
+    }
+    public boolean boundOff(Pinball ball)
+    {
+        return false;
+    }
+
+    void draw(Graphics g)
+    {
+
+    }
+
+}
